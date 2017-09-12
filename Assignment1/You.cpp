a6eb6566f8f583029e692f26a67268a029b2e941
Siming You <yous1@unlv.nevada.edu>
2017-09-12 16:34:40 -0700

    You.cpp for Assignment1 is added

---
+++ Assignment1/You.cpp
@@ -0,0 +1,15 @@
+// CS 472 Assignment #1 - Hello World
+// Siming You
+#include <iostream>
+#include <string> 
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
+
